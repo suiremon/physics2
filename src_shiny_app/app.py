d1eb87f1b6a5c3cb8edfb9f79189e11c855d2fc2
suiremon <suiremon@yandex.ru>
2024-09-13 15:27:40 +0300

    commit

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -1,6 +1,14 @@
 from shiny.express import input, render, ui
 
 import numpy as np
+
+def find_t(h,v,a):
+    d = v**2*np.sin(a)**2+4*9.81/2*h
+    t1=(-v*np.sin(a)+np.sqrt(d))/(-9.81)
+    t2=(-v*np.sin(a)-np.sqrt(d))/(-9.81)
+    return [t1,t2]
+
+    
 ui.input_text("angle", label="Введите угол:")
 ui.input_text("velocity", label="Введите скорость:")
 ui.input_text("height", label="Введите высоту:")
@@ -14,15 +22,16 @@ with ui.card(full_screen=True):
         velocity = input.velocity()
         height = input.height()
         if (angle != "" and velocity != "" and height != ""):
-            angle = np.radians(int(angle))
             velocity = int(velocity)
             height = int(height)
+            angle = np.radians(int(angle))
+            time_of_flight = find_t(height, velocity, angle)[1]
         else:
             return plt.plot(0, 0)
-        time_of_flight = 2 * velocity * np.sin(angle) / g
         time = np.linspace(0, time_of_flight, 100)
         x = velocity * np.cos(angle) * time
         y = height + velocity * np.sin(angle) * time - 0.5 * g * time**2
+
         plt.xlabel("Горизонтальное расстояние (м)")
         plt.ylabel("Высота (м)")
         plt.title("Баллистическая кривая")
