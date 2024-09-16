be833e65d133a0fbd94ecb0395776e595153d66a
suiremon <suiremon@yandex.ru>
2024-09-16 16:05:57 +0300

    commit

--- src_shiny_app/app.py
+++ src_shiny_app/app.py
@@ -33,8 +33,35 @@ with ui.card(full_screen=True):
         x = velocity * np.cos(angle) * time
         y = height + velocity * np.sin(angle) * time - 0.5 * g * time**2
 
-        plt.xlabel("Горизонтальное расстояние (м)")
-        plt.ylabel("Высота (м)")
-        plt.title("Баллистическая кривая")
-        plt.grid(True)
-        return plt.plot(x, y)
+        vx = velocity * np.cos(angle)
+        vy = velocity * np.sin(angle) - g * time
+        v = np.sqrt(vx**2 + vy**2)
+
+        fig, axis = plt.subplots(2, 2)
+
+        axis[0,0].plot(time,v)
+        axis[0,0].set_xlabel("Время (с)")
+        axis[0,0].set_ylabel("Скорость (м/с)")
+        axis[0,0].set_title("Зависимость скорости от времени")
+        axis[0,0].grid(True)
+        axis[0,0].plot(time,v)
+
+        axis[0,1].plot(x, y)
+        axis[0,1].set_xlabel("Горизонтальное расстояние (м)")
+        axis[0,1].set_ylabel("Высота (м)")
+        axis[0,1].set_title("Баллистическая кривая")
+        axis[0,1].grid(True)
+
+        axis[1, 0].plot(time, x)
+        axis[1, 0].set_ylabel("Координата х (м)")
+        axis[1, 0].set_xlabel("Время (с)")
+        axis[1, 0].set_title("Зависимость координаты х от времени")
+        axis[1, 0].grid(True)
+
+        axis[1, 1].plot(time, y)
+        axis[1, 1].set_ylabel("Координата y (м)")
+        axis[1, 1].set_xlabel("Время (с)")
+        axis[1, 1].set_title("Зависимость координаты y от времени")
+        axis[1, 1].grid(True)
+        return fig
+
